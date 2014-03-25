913bb3bb487d32badcf0aeeddef8b39d404c6c82
Luke Barratt <luke@barratt.me>
2014-03-25 11:15:49 +0000

    Updated unicorn config Updated gem versions Removed foreman

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock '3.1.0'
 set :application, 'ensl'
 set :deploy_user, 'deploy'
 set :deploy_to, '/var/www/virtual/ensl.org/deploy'
+set :deploy_via, :remote_cache
 set :pty, true
 
 set :scm, :git
@@ -11,14 +12,18 @@ set :keep_releases, 10
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.1.1'
-set :rbenv_sudo, "sudo /home/#{fetch(:deploy_user)}/.rbenv/bin/rbenv sudo"
 
+set :unicorn_config_path, "config/unicorn.rb"
+set :unicorn_rack_env, fetch(:rails_env)
+
+set :writable_dirs, %w{public tmp}
 set :linked_files, %w{.env}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle 
                      public/system public/local public/uploads}
 
-set :writable_dirs, %w{public tmp}
-set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
+set :normalize_asset_timestamps, %{public/images 
+                                   public/javascripts
+                                   public/stylesheets}
 
 namespace :deploy do
   namespace :check do
@@ -38,43 +43,9 @@ namespace :deploy do
     end
   end
 
-  desc 'Restart application'
   task :restart do
-    invoke 'foreman:export'
-    invoke 'foreman:restart'
+    invoke 'unicorn:restart'
   end
 
   after :publishing, :restart
-end
-
-namespace :foreman do
-  desc "Export the Procfile to Ubuntu's upstart scripts"
-  task :export do
-    on roles(:app) do |host|
-      within release_path do
-        execute fetch(:rbenv_sudo), "bundle exec foreman export upstart /etc/init -a #{fetch(:application)} -u #{fetch(:deploy_user)} -l #{fetch(:deploy_to)}/shared/log"
-      end
-    end
-  end
-
-  desc "Start the application services"
-  task :start do
-    on roles(:app) do |host|
-      execute "sudo service #{fetch(:application)} start"
-    end
-  end
-
-  desc "Stop the application services"
-  task :stop do
-    on roles(:app) do |host|
-      execute "sudo service #{fetch(:application)} stop"
-    end
-  end
-
-  desc "Restart the application services"
-  task :restart do
-    on roles(:app) do |host|
-      execute "sudo service #{fetch(:application)} start || #{sudo} service #{fetch(:application)} restart"
-    end
-  end
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,25 +1,33 @@
-worker_processes Integer(ENV['UNICORN_WORKERS'] || 3)
+base_path = "/var/www/virtual/ensl.org/deploy"
+current_path = "#{base_path}/current"
+shared_path = "#{base_path}/shared"
+
+working_directory current_path
+worker_processes Integer(ENV['UNICORN_WORKERS'] || 4)
 timeout 30
 preload_app true
 
-listen ENV['UNICORN_PORT']
-listen ENV['UNICORN_SOCKET']
-
+user ENV['UNICORN_USER'], ENV['UNICORN_GROUP']
+listen Integer(ENV['UNICORN_PORT'] || 4000), :tcp_nopush => true
+listen ENV['UNICORN_SOCKET'], :backlog => 64
+ 
+stderr_path "#{shared_path}/log/unicorn.stderr.log"
+stdout_path "#{shared_path}/log/unicorn.stdout.log"
+pid "#{shared_path}/tmp/pids/unicorn.pid"
+ 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+  ActiveRecord::Base.connection.disconnect!
+ 
+  old_pid = "#{server.config[:pid]}.oldbin"
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill('QUIT', File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
 end
-
+ 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection
 end
