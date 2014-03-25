74bb409a71771c10a13b7ab133373d7f6636a3e9
Luke Barratt <luke@barratt.me>
2014-03-25 14:44:42 +0000

    Updated unicorn and capistrano configs

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,6 @@
 set :branch, 'develop'
 set :rails_env, 'staging'
+set :unicorn_rack_env, fetch(:rails_env)
 
 role :app, %w{deploy@staging.ensl.org}
 role :web, %w{deploy@staging.ensl.org}
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,8 @@
 require File.expand_path('../application', __FILE__)
-require 'dotenv'
 require 'verification'
 require 'exceptions'
 require 'extra'
 
-Dotenv.load
 ActiveSupport::Deprecation.silenced = true if ['staging', 'production'].include?(ENV['RAILS_ENV'])
 
 Ensl::Application.initialize!
