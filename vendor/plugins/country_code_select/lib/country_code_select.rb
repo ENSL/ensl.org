e1a42da0dc2bc6477bcf40344a5b468ffc990443
Luke Barratt <luke@barratt.me>
2014-03-30 23:32:11 +0100

    Removed dynamic_form and country_code_select gems to fold them in as a gem dependencies.

--- vendor/plugins/country_code_select/lib/country_code_select.rb
+++
@@ -1,8 +0,0 @@
-require 'country_code_select/countries'
-require 'country_code_select/form_builder'
-require 'country_code_select/form_helpers'
-require 'country_code_select/instance_tag'
-
-ActionView::Base.send(:include, CountryCodeSelect::FormHelpers)
-ActionView::Helpers::InstanceTag.send(:include, CountryCodeSelect::InstanceTag)
-ActionView::Helpers::FormBuilder.send(:include, CountryCodeSelect::FormBuilder)
