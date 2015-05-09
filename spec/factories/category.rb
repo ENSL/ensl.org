473d9ea15b326d38feed514a9d420a19348fcd0d
cblanc <cablanchard@gmail.com>
2015-05-09 16:21:18 +0100

    Added necessary factory methods to test shoutbox

--- spec/factories/category.rb
+++ spec/factories/category.rb
@@ -3,4 +3,12 @@ FactoryGirl.define do
     sequence(:name) { |n| "Category #{n}" }
     sequence(:sort) { |n| n }
   end
+
+  trait :news do
+  	domain Category::DOMAIN_NEWS
+  end
+
+  trait :game do
+  	domain Category::DOMAIN_GAMES
+  end
 end
