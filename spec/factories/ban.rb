15ed4e70a963d321076df6e2d9ef5a5098181bf3
Chris Blanchard <chris@iddqd.org>
2015-06-02 17:55:08 +0100

    Remove rcon from annotate and added message model specs

---
+++ spec/factories/ban.rb
@@ -0,0 +1,23 @@
+FactoryGirl.define do
+	factory :ban do
+		ban_type Ban::TYPE_SITE
+		expiry Date.today + 1
+		# Hack because of the awkward way bans are created (requires user_name)
+		before(:create) do |ban|
+			if ban.user.nil?
+				user = create :user
+				ban.user_name = user.username
+			else 
+				ban.user_name = ban.user.username
+			end
+		end
+	end
+
+	trait :mute do
+		ban_type Ban::TYPE_MUTE
+	end
+
+	trait :expired do
+		expiry Date.yesterday - 1
+	end
+end
