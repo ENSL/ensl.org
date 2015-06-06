46c6c89aec66a769550f0686fee514d2a4ef7e4a
Christopher Blanchard <cablanchard@epicurus.home>
2015-06-07 00:58:41 +0100

    Added issue feature spec

---
+++ spec/factories/issue.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :issue do
+		sequence(:title) { |n| "Issue title #{n}"  } 
+		sequence(:text) { |n| "Issue Text #{n}"  } 
+		status Issue::STATUS_OPEN
+		association :author, factory: :user
+	end
+end
