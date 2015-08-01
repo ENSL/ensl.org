3ec284b92564b7142f84f5f44a32ab1fb6f4668d
Chris Blanchard <cablanchard@gmail.com>
2015-08-01 16:34:01 +0100

    Added maps api

---
+++ spec/controllers/api/v1/maps_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Api::V1::MapsController do
+  before do
+    request.accept = 'application/json'
+  end 
+
+  describe '#index' do
+    let!(:map) { create :map }
+
+    it 'returns a list of maps' do
+      get :index
+      expect(response).to be_success
+      expect(json['maps'].length).to eq(1)
+      json_map = json['maps'][0]
+      expect(json_map['id']).to eq(map.id)
+    end
+  end
+end
