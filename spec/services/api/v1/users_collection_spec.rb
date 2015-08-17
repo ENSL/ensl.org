2665df6b71f359885f89ec258c3b631152fd69fe
Chris Blanchard <cablanchard@gmail.com>
2015-08-17 19:16:32 +0100

    Fix style

--- spec/services/api/v1/users_collection_spec.rb
+++ spec/services/api/v1/users_collection_spec.rb
@@ -1,29 +1,29 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Api::V1::UsersCollection do
   let(:collection) { Api::V1::UsersCollection.new }
 
-  describe '#execute_query' do
-    describe 'when there are users with no teams' do
+  describe "#execute_query" do
+    describe "when there are users with no teams" do
       before do
         3.times { create(:user) }
       end
 
-      it 'returns 0 results' do
+      it "returns 0 results" do
         expect(collection.execute_query.size).to eq(0)
       end
     end
 
-    describe 'when there are some users with teams' do
+    describe "when there are some users with teams" do
       before do
         3.times { create(:user_with_team) }
       end
 
-      it 'returns 3 results' do
+      it "returns 3 results" do
         expect(collection.execute_query.size).to eq(3)
       end
 
-      it 'returns 6 columns' do
+      it "returns 6 columns" do
         expect(collection.execute_query.first.size).to eq(6)
       end
     end
