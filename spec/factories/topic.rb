e94bbba48dc0fc78a0309988a916536163951be2
Chris Blanchard <cablanchard@gmail.com>
2015-08-20 15:58:10 +0100

    Fix

--- spec/factories/topic.rb
+++ spec/factories/topic.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :topic do
     sequence(:title) { |n| "Forum Title #{n}" }
+    forum
+    user
+    before(:create) do |topic|
+    	topic.first_post = "My first post on the topic"
+    end
   end
 end
