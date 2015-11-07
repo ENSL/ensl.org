5825ac33f5d4830aa7b504423a3965000e1cc8e7
Prommah <prommah@prommah.com>
2015-11-07 06:21:09 +0000

    Add match-related factories

---
+++ spec/factories/match.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :match do
+    contest
+    contester1 do
+      create(:contester, contest: contest)
+    end
+    contester2 do
+      create(:contester, contest: contest)
+    end
+
+    match_time 1.hour.from_now
+  end
+end
