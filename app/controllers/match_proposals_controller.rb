29a6a838874a44fcd1c9ac8d270e52278f9979d1
Absurdon <peter@brodkorb-web.de>
2017-06-11 16:36:10 +0200

    Added submit function and processing

--- app/controllers/match_proposals_controller.rb
+++ app/controllers/match_proposals_controller.rb
@@ -16,15 +16,32 @@ class MatchProposalsController < ApplicationController
     @proposal.team = cuser.team
     @proposal.status = MatchProposal::STATUS_PENDING
 
-    if @proposal.save!
+    if @proposal.save
       flash[:notice] = 'Created new proposal'
-      redirect_to(@match)
+      redirect_to(match_proposals_path(@match))
     else
       render :new
     end
   end
 
   def update
+    raise AccessError unless @match.can_make_proposal?(cuser)
+    @proposal = MatchProposal.find(params[:id])
+    @proposal.status = params[:match_proposal][:status]
+    if @proposal.save
+      action = case @proposal.status
+                 when MatchProposal::STATUS_CONFIRMED
+                   "Confirmed Proposal for #{Time.use_zone(view_context.timezone_offset) { @proposal.proposed_time.strftime('%d %B %y %H:%M %Z') }}"
+                 when MatchProposal::STATUS_REJECTED
+                   "Rejected Proposal for #{Time.use_zone(view_context.timezone_offset) { @proposal.proposed_time.strftime('%d %B %y %H:%M %Z') }}"
+                 else
+                   "Smthn went wrong"
+               end
+      flash[:notice] = action
+    else
+      flash[:notice] = "Error"
+    end
+    redirect_to(match_proposals_path(@match))
   end
 
 private
