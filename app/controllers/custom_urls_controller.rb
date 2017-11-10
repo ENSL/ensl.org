1bb40705ab0dc36d4bfb2b7f08dc3726a7106f27
Absurdon <peter@brodkorb-web.de>
2017-11-10 10:42:35 +0100

    Implemented #show

--- app/controllers/custom_urls_controller.rb
+++ app/controllers/custom_urls_controller.rb
@@ -3,9 +3,15 @@ class CustomUrlsController < ApplicationController
   end
 
   def create
+
   end
 
   def show
+    custom_url = CustomUrl.find_by_name(params[:name])
+    @article = custom_url.article
+    raise AccessError unless @article.can_show? cuser
+    @article.read_by! cuser if cuser
+    render 'articles/show'
   end
 
   def update
