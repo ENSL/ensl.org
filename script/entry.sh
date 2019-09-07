82d1dc7355f4eb1290e69144e3fe06f31e89a90c
Ari Timonen <arit@fastmail.com>
2019-09-07 21:46:30 +0300

    Add compiling master

--- script/entry.sh
+++ script/entry.sh
@@ -7,5 +7,5 @@ rm -rf /var/www/public/assets
 mv /home/web/assets /var/www/public/
 chown -R web:web /var/www/public
 
-#bundle exec rake assets:precompile
+su -c "cd /var/www && bundle exec rake assets:precompile" -s /bin/bash -l web
 su -c "cd /var/www && bundle exec puma -C config/puma.rb" -s /bin/bash -l web
