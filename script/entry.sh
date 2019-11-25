2c34da556aeeeb8b4d1f01e587fd18e59376bf06
Absurdon <peter@brodkorb-web.de>
2019-11-25 14:11:38 +0100

    Make sure the logfile exists

--- script/entry.sh
+++ script/entry.sh
@@ -7,5 +7,9 @@ rm -rf /var/www/public/assets
 mv /home/web/assets /var/www/public/
 chown -R web:web /var/www/public
 
+LOG_FILE = "/var/www/log/$RAILS_ENV.log"
+touch $LOG_FILE
+chown web:web $LOG_FILE
+
 su -c "cd /var/www && bundle exec rake assets:precompile" -s /bin/bash -l web
 su -c "cd /var/www && bundle exec puma -C config/puma.rb" -s /bin/bash -l web
