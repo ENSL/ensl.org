9a6db8913907812b4aa6a716ead6a8e4ef555f82
Ari Timonen <arit@fastmail.com>
2020-03-18 05:53:19 +0200

    Fix bug on user page slow way

--- app/models/match.rb
+++ app/models/match.rb
@@ -90,7 +90,7 @@ class Match < ActiveRecord::Base
   scope :of_contester, -> (contester) { where("contester1_id = ? OR contester2_id = ?", contester.id, contester.id) }
   scope :of_user, -> (user) { includes(:matchers).where("matchers.user_id = ?",  user.id) }
   scope :of_team, -> (team) { includes({:contester1 => :team, :contester2 => :team}).where("teams.id = ? OR teams_contesters.id = ?", team.id, team.id) }
-  scope :of_userteam, -> (user, team) { includes({:matchers => {:contester => :team}}).where("teams.id = ? AND matchers.user_id = ?", team.id, user.id) }
+  scope :of_userteam, -> (user, team) { eager_load({:matchers => {:contester => :team}}).where("teams.id = ? AND matchers.user_id = ?", team.id, user.id) }
   scope :within_time, -> (from, to) { where("match_time > ? AND match_time < ?", from.utc, to.utc) }
   scope :around, -> (time) { where("match_time > ? AND match_time < ?", time.ago(MATCH_LENGTH).utc, time.ago(-MATCH_LENGTH).utc) }
   scope :after, -> (time) { where("match_time > ? AND match_time < ?", time.utc, time.ago(-MATCH_LENGTH).utc) }
