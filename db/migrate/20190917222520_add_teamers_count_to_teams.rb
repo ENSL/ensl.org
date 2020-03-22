93ff33080056f57a07d4cb5c479aec9a69f93281
Ari Timonen <arit@fastmail.com>
2020-03-22 19:46:17 +0200

    Update version number for migrations

--- db/migrate/20190917222520_add_teamers_count_to_teams.rb
+++ db/migrate/20190917222520_add_teamers_count_to_teams.rb
@@ -1,4 +1,4 @@
-class AddTeamersCountToTeams < ActiveRecord::Migration
+class AddTeamersCountToTeams < ActiveRecord::Migration[4.2]
   def change
     add_column :teams, :teamers_count, :integer
   end
