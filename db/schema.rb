3569f52d4ab45a9468dee9c8e242a83285af83fe
Ari Timonen <arit@fastmail.com>
2020-04-02 03:26:17 +0300

    Add missing schema.rb for read_marks migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_031046) do
+ActiveRecord::Schema.define(version: 2020_04_02_000314) do
 
   create_table "article_versions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "article_id"
