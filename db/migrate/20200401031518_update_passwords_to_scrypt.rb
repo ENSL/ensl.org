7ae3487d5938b1a594034af1ba51064080cce4de
Ari Timonen <arit@fastmail.com>
2020-04-02 03:15:28 +0300

    Update scrypt migration

--- db/migrate/20200401031518_update_passwords_to_scrypt.rb
+++ db/migrate/20200401031518_update_passwords_to_scrypt.rb
@@ -1,13 +1,18 @@
+
+ENV['SCRYPT_MAX_TIME'] ||= "0.03"
 class UpdatePasswordsToScrypt < ActiveRecord::Migration[6.0]
   require 'scrypt'
   require 'user'
 
   def up
-    SCrypt::Engine.calibrate!(max_time: 0.03)
+    SCrypt::Engine.calibrate!(max_time: ENV['SCRYPT_MAX_TIME'])
     ActiveRecord::Base.transaction do
       User.all.order(:id).each do |user|
-        user.update_password
-        user.save!
+        user.team = nil unless user&.team&.present?
+        if user.valid?
+          user.update_password
+          user.save!
+        end
       end
     end
   end
