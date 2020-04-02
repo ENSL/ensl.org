5c60ee7013ef0b04a4e10e0987c156e9134c3e64
Ari Timonen <arit@fastmail.com>
2020-04-02 03:16:03 +0300

    Fix migration for readings

--- db/migrate/20200402000314_move_readings_to_read_marks.rb
+++ db/migrate/20200402000314_move_readings_to_read_marks.rb
@@ -2,7 +2,7 @@ class MoveReadingsToReadMarks < ActiveRecord::Migration[6.0]
   def change
     # execute "TRUNCATE read_marks"
     execute "INSERT INTO read_marks (readable_type, readable_id, reader_type, reader_id, timestamp)
-             SELECT readable_type, readable_id, "User", user_id, updated_at
+             SELECT readable_type, readable_id, 'User', user_id, updated_at
              FROM readings;"
   end
 end
