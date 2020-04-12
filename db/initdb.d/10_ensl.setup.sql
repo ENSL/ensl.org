88a6c697ccd1286041aa66edff99547408416cd3
Ari Timonen <arit@fastmail.com>
2020-04-12 13:39:10 +0300

    Fix MariaDB issues
    Use different root password
    Use correct database for setup
    Change precedence for setup SQL scripts
    Remove deprececated options from my.cnf

--- db/initdb.d/10_ensl.setup.sql
+++ db/initdb.d/10_ensl.setup.sql
@@ -1,9 +1,9 @@
 -- FIXME: this should be somewhere else probably
-CREATE DATABASE ensl_development;
+CREATE DATABASE IF NOT EXISTS ensl_development;
 GRANT ALL PRIVILEGES ON ensl_development.* TO 'ensl'@'%' WITH GRANT OPTION;
 
-CREATE DATABASE ensl_test;
+CREATE DATABASE IF NOT EXISTS ensl_test;
 GRANT ALL PRIVILEGES ON ensl_test.* TO 'ensl'@'%' WITH GRANT OPTION;
 
-CREATE DATABASE ensl_staging;
+CREATE DATABASE IF NOT EXISTS ensl_staging;
 GRANT ALL PRIVILEGES ON ensl_staging.* TO 'ensl'@'%' WITH GRANT OPTION;
