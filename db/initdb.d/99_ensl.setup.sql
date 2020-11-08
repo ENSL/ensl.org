4ff9a1fc20433706bcc06d361059fcab2ee6b316
Ari Timonen <arit@fastmail.com>
2020-11-08 17:16:00 +0100

    Add a setup script for DB

---
+++ db/initdb.d/99_ensl.setup.sql
@@ -0,0 +1,12 @@
+-- FIXME: this should be somewhere else probably
+CREATE DATABASE IF NOT EXISTS ensl;
+GRANT ALL PRIVILEGES ON ensl.* TO 'ensl'@'%' WITH GRANT OPTION;
+
+CREATE DATABASE IF NOT EXISTS ensl_development;
+GRANT ALL PRIVILEGES ON ensl_development.* TO 'ensl'@'%' WITH GRANT OPTION;
+
+CREATE DATABASE IF NOT EXISTS ensl_test;
+GRANT ALL PRIVILEGES ON ensl_test.* TO 'ensl'@'%' WITH GRANT OPTION;
+
+CREATE DATABASE IF NOT EXISTS ensl_staging;
+GRANT ALL PRIVILEGES ON ensl_staging.* TO 'ensl'@'%' WITH GRANT OPTION;
