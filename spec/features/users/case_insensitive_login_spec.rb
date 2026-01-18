1043233eb0b42237b1e02f5d92000634862fe970
Ari Timonen <arit@fastmail.com>
2026-01-18 01:55:10 +0000

    Code style fixes

--- spec/features/users/case_insensitive_login_spec.rb
+++ spec/features/users/case_insensitive_login_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
-feature "Case insensitive login", js: :true do
-  let(:username) { "CaSe_InSeNsItIvE" }
-  let(:password) { "passwordABC123" }
+feature 'Case insensitive login', js: :true do
+  let(:username) { 'CaSe_InSeNsItIvE' }
+  let(:password) { 'passwordABC123' }
   let!(:user) { create(:user, username: username, raw_password: password) }
 
   before do
     visit root_path
   end
 
-  feature "when a user with mixed-case username signs in" do
-    scenario "with a matching case allows the user to sign in" do
+  feature 'when a user with mixed-case username signs in' do
+    scenario 'with a matching case allows the user to sign in' do
       fill_login_form(username)
-      find('#authentication input[name="commit"]').click()
+      find('#authentication input[name="commit"]').click
 
-      expect(page).to have_content(I18n.t("login_successful"))
+      expect(page).to have_content(I18n.t('login_successful'))
 
       within user_status do
         expect(page).to have_content(account_link)
       end
     end
 
-    scenario "with a non-matching case allows the user to sign in" do
-      fill_login_form("CASE_INSENSITIVE")
-      find('#authentication input[name="commit"]').click()
+    scenario 'with a non-matching case allows the user to sign in' do
+      fill_login_form('CASE_INSENSITIVE')
+      find('#authentication input[name="commit"]').click
 
-      expect(page).to have_content(I18n.t("login_successful"))
+      expect(page).to have_content(I18n.t('login_successful'))
 
       within user_status do
         expect(page).to have_content(account_link)
@@ -34,11 +34,11 @@ feature "Case insensitive login", js: :true do
   end
 
   def fill_login_form(username)
-    fill_in "login_username", with: username
-    fill_in "login_password", with: password
+    fill_in 'login_username', with: username
+    fill_in 'login_password', with: password
   end
 
   def account_link
-    "ACCOUNT"
+    'ACCOUNT'
   end
 end
