086e484835385624455a5bb38e9ac4e896565237
Ari Timonen <arit@fastmail.com>
2026-01-19 01:28:28 +0000

    Improve missing DOM issue in rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,7 +96,7 @@ end
 Capybara.server = :puma
 Capybara.default_driver = :rack_test
 Capybara.javascript_driver = :selenium_chrome_headless
-Capybara.default_max_wait_time = 1
+Capybara.default_max_wait_time = 10
 
 # require "capybara/poltergeist"
 # Capybara.javascript_driver = :apparition
