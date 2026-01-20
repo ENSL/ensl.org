b7c1359344df25afafa9a9d11b2741afdfab34d8
Ari Timonen <arit@fastmail.com>
2026-01-20 01:36:57 +0000

    Add some basic comment testing

--- spec/factories/contest.rb
+++ spec/factories/contest.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :contest do
     sequence(:name) { |n| "Contest ##{n}" }
-
-    start Date.yesterday
-    self.end Date.tomorrow
-    status Contest::STATUS_PROGRESS
-    default_time "12:00:00"
+    start { 1.day.ago }
+    add_attribute(:end) { 1.day.from_now }
+    status { Contest::STATUS_OPEN }
+    default_time { "12:00:00" }
   end
 end
