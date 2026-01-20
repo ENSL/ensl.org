012226ab2b87dd97147d48abd08ef7f6cdb77839
Ari Timonen <arit@fastmail.com>
2026-01-20 00:28:37 +0000

    But coverage behind env var

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,22 +7,22 @@ require 'dotenv'
 Dotenv.load('.env.' + ENV['RAILS_ENV'] + '.local', '.env.local', '.env.' + ENV['RAILS_ENV'], '.env')
 
 # require 'codeclimate-test-reporter'
-require 'simplecov'
 
-# require 'capybara/apparition'
+if ENV['SIMPLECOV'] == '1'
+  require 'simplecov'
 
-# CodeClimate::TestReporter.start
-SimpleCov.start "rails" do
-  enable_coverage :branch
-  add_filter %r{^/spec/}
-  add_filter %r{^/config/}
-  add_filter %r{^/db/}
-  add_filter %r{^/vendor/}
+  SimpleCov.start "rails" do
+    enable_coverage :branch
+    add_filter %r{^/spec/}
+    add_filter %r{^/config/}
+    add_filter %r{^/db/}
+    add_filter %r{^/vendor/}
 
-  add_group "Controllers", "app/controllers"
-  add_group "Models",      "app/models"
-  add_group "Jobs",        "app/jobs"
-  add_group "Services",    "app/services"
+    add_group "Controllers", "app/controllers"
+    add_group "Models",      "app/models"
+    add_group "Jobs",        "app/jobs"
+    add_group "Services",    "app/services"
+  end
 end
 
 require File.expand_path('../config/environment', __dir__)
