eac8648556e6437ecb105496cbb5ba8b3cef9390
Ari Timonen <arit@fastmail.com>
2026-01-21 01:58:27 +0000

    Add asset compilation for tests
    Fixes CSS issue if they are not there for feature tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,12 +11,30 @@ require 'capybara/rspec'
 # HTML 5 validation
 require 'capybara/validate_html5'
 
+def ensure_test_assets_precompiled!
+  return if ENV['SKIP_ASSET_PRECOMPILE'].present?
+
+  assets_dir = Rails.root.join('public', 'assets')
+  manifest = Dir[assets_dir.join('.sprockets-manifest*.json')].first ||
+             Dir[assets_dir.join('manifest*.json')].first
+
+  return if manifest && File.exist?(manifest)
+
+  puts 'Precompiling assets for test environment...'
+  success = system({ 'RAILS_ENV' => 'test' }, 'bin/rails', 'assets:precompile')
+  abort('Assets precompile failed in test environment.') unless success
+end
+
 ActiveRecord::Migration.maintain_test_schema!
 
 # load support files
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    ensure_test_assets_precompiled!
+  end
+
   config.before(:each) do
     Rails.cache.clear
   end
