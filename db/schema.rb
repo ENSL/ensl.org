7f6f264a63ef365fc160fa8887044ee684001e7d
Ari Timonen <arit@fastmail.com>
2026-01-31 15:23:16 +0000

    Update schema ID

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_29_000000) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_31_132000) do
   create_table "article_versions", id: :integer, charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
     t.integer "article_id"
     t.datetime "created_at", precision: nil
