8c06fa0d56565c5bc36106b063a3b9e89bd7dfb9
Ari Timonen <arit@fastmail.com>
2026-02-06 18:14:11 +0000

    Feature spec for locks

--- app/models/lock.rb
+++ app/models/lock.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: locks
@@ -13,19 +15,21 @@
 #  index_locks_on_lockable_id_and_lockable_type  (lockable_id,lockable_type)
 #
 
+# Model for locks on lockable objects. Used to prevent multiple
+# users from posting in locked threads, matches etc.
 class Lock < ActiveRecord::Base
   include Extra
-  belongs_to :lockable, :polymorphic => true, :optional => true
+  belongs_to :lockable, polymorphic: true, optional: true
 
-  def can_create? cuser
-    cuser and cuser.admin?
+  def can_create?(cuser)
+    cuser&.admin?
   end
 
-  def can_destroy? cuser
-    cuser and cuser.admin?
+  def can_destroy?(cuser)
+    cuser&.admin?
   end
 
-  def self.params(params, cuser)
+  def self.params(params, _cuser)
     params.require(:lock).permit(:lockable_type, :lockable_id)
   end
 end
---
+++ spec/features/admin/locks_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Topic locks', type: :feature, js: true do
+  let(:admin) { create(:user, :admin) }
+  let(:user) { create(:user) }
+  let!(:forum) { create(:forum) }
+  let!(:topic) { create(:topic, forum: forum, user: user) }
+
+  before do
+    Capybara.javascript_driver = :selenium_chrome_headless
+  end
+
+  scenario 'admin locks and unlocks a topic from the topic page' do
+    sign_in_via_session(admin)
+    visit topic_path(topic)
+
+    expect(page).to have_link('Reply')
+    expect(page).to have_link('Lock')
+
+    click_link 'Lock'
+
+    expect(page).to have_content(I18n.t(:topics_locked))
+    expect(page).to have_link('Unlock')
+    expect(page).not_to have_link('Lock')
+    expect(page).not_to have_link('Reply')
+
+    click_link 'Unlock'
+
+    expect(page).to have_link('Lock')
+    expect(page).not_to have_link('Unlock')
+  end
+
+  scenario 'regular user does not see lock controls' do
+    sign_in_via_session(user)
+    visit topic_path(topic)
+
+    expect(page).not_to have_link('Lock')
+    expect(page).not_to have_link('Unlock')
+  end
+
+  scenario 'forum topic list shows locked label' do
+    Lock.create!(lockable: topic)
+
+    sign_in_via_session(user)
+    visit forum_path(forum)
+
+    expect(page).to have_content('Locked:')
+    expect(page).to have_link(topic.title)
+  end
+end
