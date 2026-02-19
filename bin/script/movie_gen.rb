fe4ef3d4ba35ee8fbcfc6ccc0a49aaf270a1eedc
Ari Timonen <arit@fastmail.com>
2026-02-19 23:58:42 +0000

    Add a simple script to update movies

---
+++ bin/script/movie_gen.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../../config/environment'
+
+scope = Movie.includes(:file, :preview).order(:id)
+total = scope.count
+started_at = Time.now
+tick = (ENV['PROGRESS_EVERY'] || 25).to_i
+tick = 25 if tick <= 0
+
+stats = Hash.new(0)
+
+puts "Movies: #{total}"
+
+scope.find_each.with_index(1) do |movie, i|
+  source = movie.processable_source_path
+
+  unless source
+    stats[:skipped_missing_file] += 1
+    if (i % tick).zero? || i == total
+      puts "[#{i}/#{total}] snap=#{stats[:snapshots]} prev=#{stats[:previews]} skip=#{stats[:skipped_missing_file]} fail=#{stats[:failed]}"
+    end
+    next
+  end
+
+  unless movie.snapshot?
+    if movie.make_snapshot
+      stats[:snapshots] += 1
+    else
+      stats[:failed] += 1
+    end
+  end
+
+  needs_preview = !movie.web_friendly && movie.preview.blank? && !File.exist?(movie.preview_path)
+  if needs_preview
+    begin
+      movie.make_preview
+      stats[:previews] += 1
+    rescue StandardError
+      stats[:failed] += 1
+    end
+  end
+
+  if (i % tick).zero? || i == total
+    puts "[#{i}/#{total}] snap=#{stats[:snapshots]} prev=#{stats[:previews]} skip=#{stats[:skipped_missing_file]} fail=#{stats[:failed]}"
+  end
+end
+
+elapsed = (Time.now - started_at).round
+puts "Done in #{elapsed}s | snapshots=#{stats[:snapshots]} previews=#{stats[:previews]} skipped_missing_file=#{stats[:skipped_missing_file]} failed=#{stats[:failed]}"
