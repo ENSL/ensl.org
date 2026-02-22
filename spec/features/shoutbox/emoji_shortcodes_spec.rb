f34cf6b21f0b14d7483e89d727112f9d0051a975
Ari Timonen <arit@fastmail.com>
2026-02-22 09:51:17 +0000

    Spec emojis

---
+++ spec/features/shoutbox/emoji_shortcodes_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+feature 'Shoutbox emoji shortcodes', js: true do
+  let!(:user) { create :user }
+
+  background do
+    sign_in_via_session(user)
+    visit root_path
+    ActiveJob::Base.queue_adapter = :inline
+  end
+
+  scenario 'shortcodes are converted and shout input continues working after submit' do
+    visit root_path
+    expect(page).to have_selector('turbo-cable-stream-source[connected]', visible: :all, wait: 10)
+
+    fill_in 'shoutbox_text', with: ':smile:'
+    click_button 'Shout!'
+    expect(page).to have_field('shoutbox_text', with: '', wait: 5)
+
+    expect(Shoutmsg.order(:id).last&.text).to eq('😄')
+
+    fill_in 'shoutbox_text', with: ':heart:'
+    click_button 'Shout!'
+    expect(page).to have_field('shoutbox_text', with: '', wait: 5)
+
+    latest_two_texts = Shoutmsg.order(id: :desc).limit(2).pluck(:text)
+    expect(latest_two_texts).to include('😄', '❤️')
+  end
+
+  scenario 'autocomplete menu appears and inserts selected shortcode token' do
+    visit root_path
+    expect(page).to have_selector('turbo-cable-stream-source[connected]', visible: :all, wait: 10)
+
+    input = find_field('shoutbox_text')
+    input.send_keys(':smi')
+
+    expect(page).to have_css('.tribute-container li', text: ':smile:', wait: 5)
+    find('.tribute-container li', text: ':smile:').click
+
+    expect(find_field('shoutbox_text').value).to include(':smile:')
+  end
+end
---
+++ spec/models/emoji_shortcodes_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Emoji shortcode parsing' do
+  it 'parses shortcodes/emoticons for all main content models', :aggregate_failures do
+    user = create(:user)
+    other_user = create(:user)
+    category = create(:category)
+    article = create(:article, user: user, category: category, text: ':smile:', text_coding: Article::CODING_BBCODE)
+
+    post = create(:post, user: user, topic: create(:topic, user: user), text: ':heart:')
+    comment = Comment.create!(user: user, commentable: article, text: ':fire:')
+    message = create(:message, sender: user, recipient: other_user, text: ':tada:')
+    issue = create(:issue, author: user, text: ':rocket:')
+    shoutmsg = Shoutmsg.create!(user: user, text: ':)')
+    markdown_article = create(:article,
+                              user: user,
+                              category: category,
+                              text: ':smile:',
+                              text_coding: Article::CODING_MARKDOWN)
+
+    expect(article.text_parsed).to include('😄')
+    expect(post.text_parsed).to include('❤️')
+    expect(comment.text_parsed).to include('🔥')
+    expect(message.text_parsed).to include('🎉')
+    expect(issue.text_parsed).to include('🚀')
+    expect(shoutmsg.text).to include('😊')
+    expect(markdown_article.text_parsed).to include('😄')
+  end
+end
