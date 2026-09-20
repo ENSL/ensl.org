c97f4b28c10a619435521c23b08cbaad2dee70f7
Ari Timonen <arit@fastmail.com>
2026-09-20 00:51:26 +0000

    Fix movie link low sec issue

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module MoviesHelper
+  LOCAL_FILE_URL = %r{\A/files/(?!/)[^\r\n]*\z}
+
   def movie_rating_options
     [
       ['All', ''],
@@ -10,5 +12,10 @@ module MoviesHelper
       ['2 stars', 2],
       ['1 star', 1]
     ]
+  end
+
+  def movie_download_url(movie_file)
+    url = movie_file.url.to_s
+    url if LOCAL_FILE_URL.match?(url)
   end
 end
--- spec/helpers/movies_helper_spec.rb
+++ spec/helpers/movies_helper_spec.rb
@@ -3,6 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe MoviesHelper, type: :helper do
+  describe '#movie_download_url' do
+    it 'returns local file URLs' do
+      movie_file = instance_double(DataFile, url: '/files/movies/video.mp4')
+
+      expect(helper.movie_download_url(movie_file)).to eq('/files/movies/video.mp4')
+    end
+
+    it 'rejects non-local and malformed URLs' do
+      expect(helper.movie_download_url(instance_double(DataFile, url: 'javascript:alert(1)'))).to be_nil
+      expect(helper.movie_download_url(instance_double(DataFile, url: '//example.test/file.mp4'))).to be_nil
+      malformed_file = instance_double(DataFile, url: "/files/video.mp4\njavascript:alert(1)")
+
+      expect(helper.movie_download_url(malformed_file)).to be_nil
+    end
+  end
+
   describe '#movie_rating_options' do
     it 'returns the expected movie rating select options' do
       expect(helper.movie_rating_options).to eq(
